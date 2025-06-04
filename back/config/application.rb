a6495fe03c5e882cc87558a32105d02abf820860
takumi333 <takumiatat@yahoo.co.jp>
2025-06-04 15:32:24 +0900

    chore/#3 railsセットアップ完了

--- back/config/application.rb
+++ back/config/application.rb
@@ -21,7 +21,14 @@ module Myapp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
+
+    # Set locale
+    config.i18n.default_locale = :ja
+    config.i18n.available_locales = [:en, :ja]
+
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
