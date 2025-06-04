9e3b80e57697f096bf1620289af265ba9fd30a01
takumi333 <takumiatat@yahoo.co.jp>
2025-06-04 14:17:57 +0900

    chore: #3 tailwind css v3に戻した

---
+++ front/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",  // App Router
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
