a5900d4ca00acf3c1b0d4fb09a2d3490b2073989
takumi333 <takumiatat@yahoo.co.jp>
2025-06-29 13:13:56 +0900

    feat: #26 ルーティング作成完了

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -8,4 +8,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api do
+    namespace :v1 do
+      namespace :auth do
+        post   'guest',  to: 'guest_sessions#create'
+        delete 'logout', to: 'sessions#destroy'
+      end
+      resources :users, only: %i[create] 
+    end
+  end
 end
