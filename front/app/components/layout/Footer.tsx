667f8eaffc09060d14fc04093bfe043398b3c560
takumi333 <takumiatat@yahoo.co.jp>
2025-07-10 15:19:59 +0900

    feat: #33 footer作成完了

--- front/app/components/layout/Footer.tsx
+++ front/app/components/layout/Footer.tsx
@@ -1,8 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+      <footer className='w-full border-t px-8 py-6 bg-blue-300'>
+        {/* <div className="flex justify-center">
+          <Link href="/">プライバシーポリシー</Link>
+        </div> */}
+        <div className='flex justify-center bg-blue-300'>
+          <p>© 2025 CareLoop</p>
+        </div>
+      </footer>
   )
 }
 
--- front/app/components/layout/Header.tsx
+++ front/app/components/layout/Header.tsx
@@ -9,7 +9,7 @@ import React from 'react'
 const Header = () => {
 
   return (
-    <header className='flex w-full items-center justify-between border-b px-4 py-2'>
+    <header className='flex w-full items-center justify-between border-b px-4 py-2 bg-blue-300'>
       <Link href="/dashboard" className="font-bold">
         CareLoop
       </Link>
